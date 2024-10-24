2adaed123efdddcf420e8df42971a1c060c5221a
Alberto <albicento.ac@gmail.com>
2024-10-24 13:37:57 +0200

    refactor: minor changes

--- natrix/rules/common.py
+++ natrix/rules/common.py
@@ -2,17 +2,17 @@ from dataclasses import dataclass
 from typing import Callable, List
 
 from natrix.ast_tools import VyperASTVisitor
+from dpath import get
 
 
-@dataclass()
+@dataclass(frozen=True)
 class Rule:
     name: str
     description: str
     run: Callable
-    # TODO add an id to ignore the rule
 
 
-@dataclass()
+@dataclass(frozen=True)
 class Issue:
     file: str
     position: str
@@ -35,14 +35,14 @@ class BaseRule(VyperASTVisitor):
         return self.issues
 
     def add_issue(self, node: dict, *message_args):
-        line = node['lineno'],
-        character = node['col_offset'],
+        line = (node["lineno"],)
+        character = (node["col_offset"],)
         # Create an Issue object and append it to issues
         issue = Issue(
-            file=self.compiler_output.get('contract_name', 'unknown'),
+            file=get(self.compiler_output, "contract_name"),
             position=f"{line}:{character}",
             severity=self.severity,
             code=self.code,
-            message=self.message.format(*message_args)
+            message=self.message.format(*message_args),
         )
         self.issues.append(issue)
