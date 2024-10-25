b0cd3e7565c85f902edf1613adc354fc1b8061e5
Alberto <albicento.ac@gmail.com>
2024-10-25 17:20:05 +0200

    fix: prevent linting foundry artifacts

--- natrix/__init__.py
+++ natrix/__init__.py
@@ -1,13 +1,7 @@
 import sys
-
 import click
-
-from natrix.__version__ import __version__
-
-
-import glob
 import os
-
+from natrix.__version__ import __version__
 from natrix.rules import rules
 from natrix.ast_tools import parse_file
 
@@ -25,9 +19,19 @@ def lint_file(file_path):
         sys.exit(1)
 
 
-def find_vy_files(directory):
-    # Recursively find all .vy files in the given directory
-    return glob.glob(os.path.join(directory, "**", "*.vy"), recursive=True)
+def find_vy_files(directory, exclude_dirs):
+    # Recursively find all .vy files in the given directory, excluding specified directories
+    vy_files = []
+    for root, dirs, files in os.walk(directory):
+        # Remove excluded directories from the walk
+        dirs[:] = [d for d in dirs if os.path.join(root, d) not in exclude_dirs]
+
+        # Collect all .vy files
+        for file in files:
+            if file.endswith(".vy"):
+                vy_files.append(os.path.join(root, file))
+
+    return vy_files
 
 
 @click.command()
@@ -37,14 +41,24 @@ def find_vy_files(directory):
     required=False,
     type=click.Path(exists=True, file_okay=True, dir_okay=True, readable=True),
 )
-def main(path):
+@click.option(
+    "--exclude",
+    multiple=True,
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True),
+    help="Directories to exclude from the linting process.",
+)
+def main(path, exclude):
+    exclude_dirs = [os.path.abspath(ex_dir) for ex_dir in exclude]
+
     if path:
         if os.path.isfile(path) and path.endswith(".vy"):
             # If a specific .vy file is provided
             lint_file(path)
         elif os.path.isdir(path):
-            # If a directory is provided, find all .vy files in it
-            vy_files = find_vy_files(path)
+            # If a directory is provided, find all .vy files in it (even if the directory name ends with .vy)
+            vy_files = find_vy_files(path, exclude_dirs)
+            if not vy_files:
+                print(f"No .vy files found in the directory: {path}")
             for file in vy_files:
                 lint_file(file)
         else:
@@ -52,7 +66,7 @@ def main(path):
             sys.exit(1)
     else:
         # If no path is provided, search for .vy files in the current directory recursively
-        vy_files = find_vy_files(".")
+        vy_files = find_vy_files(".", exclude_dirs)
 
         if not vy_files:
             print("No .vy files found in the current directory.")
@@ -63,3 +77,7 @@ def main(path):
 
     print("Vyper files are lint free! 🐍")
     sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
