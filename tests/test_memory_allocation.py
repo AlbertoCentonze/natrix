a3ceea6d4d253bac2e79c58e4229b08186a9e82c
Alberto <albicento.ac@gmail.com>
2024-10-25 14:38:43 +0200

    test: refactor fixtures

--- tests/test_memory_allocation.py
+++ tests/test_memory_allocation.py
@@ -1,24 +1,7 @@
-import json
-
-import pytest
-
 from natrix.rules import MemoryExpansionRule
 
 
-@pytest.fixture()
-def fee_splitter_contract():
-    with open("tests/contracts/ast.json") as ast_raw:
-        ast = json.load(ast_raw)
-
-    with open("tests/contracts/metadata.json") as metadata_raw:
-        metadata = json.load(metadata_raw)
-
-    ast["metadata"] = metadata
-    print(ast)
-    return ast
-
-
-def test_foo(fee_splitter_contract):
+def test_fee_splitter_dispatch_fees(fee_splitter_contract):
     rule = MemoryExpansionRule()
 
     issues = rule.run(fee_splitter_contract)
