d425c9db7d597557c783ff135bd0e7014030b383
Alberto <albicento.ac@gmail.com>
2024-10-28 19:29:16 +0100

    test: new rules unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,25 @@ import pytest
 
 @pytest.fixture()
 def fee_splitter_contract():
-    with open("tests/contracts/ast.json") as ast_raw:
+    with open("tests/contracts/fee_splitter/ast.json") as ast_raw:
         ast = json.load(ast_raw)
 
-    with open("tests/contracts/metadata.json") as metadata_raw:
+    with open("tests/contracts/fee_splitter/metadata.json") as metadata_raw:
         metadata = json.load(metadata_raw)
 
     ast["metadata"] = metadata
+
+    return ast
+
+
+@pytest.fixture()
+def dummy_version_contract():
+    with open("tests/contracts/version_dummy/ast.json") as ast_raw:
+        ast = json.load(ast_raw)
+
+    with open("tests/contracts/version_dummy/metadata.json") as metadata_raw:
+        metadata = json.load(metadata_raw)
+
+    ast["metadata"] = metadata
+
     return ast
