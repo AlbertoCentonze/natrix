b52f4aeeee6e35b651208bef2076391fd1d82b0c
Alberto <albicento.ac@gmail.com>
2025-06-16 10:44:25 +0200

    test: exports codegen

--- tests/rules/test_implicit_export.py
+++ tests/rules/test_implicit_export.py
@@ -7,6 +7,8 @@ def test_implicit_export(scrvusd_oracle_contract):
     issues = rule.run(scrvusd_oracle_contract)
     assert len(issues) == 1
     assert issues[0].position == "17:0"
-    assert issues[0].message, (
-        "Module 'ownable' is exposing all its functions. Consider exporting them one by one to makethe contract more explicit."
+    assert issues[0].message == (
+        "Module 'ownable' is exposing all its functions using `__interface__`. "
+        "Consider exporting them one by one to make the contract more explicit. "
+        "You can run 'natrix codegen exports path/to/ownable.vy' to generate the explicit exports."
     )
